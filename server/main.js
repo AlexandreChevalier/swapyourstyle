cac283037a3d2bb6958742c50a68b07dec2e354a
Alexandre Chevalier <dev.alexandre.chevalier@gmail.com>
2016-06-16 16:44:28 +0200

    Rajout package Geocoder Réorganisation des blocks Mise sous forme de template du bloc d'infos, sous la présentation : blocks/infoBlock

--- server/main.js
+++ server/main.js
@@ -1,1 +1,10 @@
 import '../imports/api/tasks.js';
+
+// Reverse
+// var geo = new GeoCoder({
+//   geocoderProvider: "mapquest",
+//   httpAdapter: "https",
+//   apiKey: 'YOUR_API_KEY'
+// });
+// var result = geo.reverse(45.767, 4.833);
+// console.log(result);
