6faefeef3265d3f60655183841e6257073298647
Marc Gilbert <marcgilbert07@gmail.com>
2016-07-12 17:27:16 +0200

    Ajout geolocalisation

--- imports/ui/pages/home-page.js
+++ imports/ui/pages/home-page.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
+import { Session } from 'meteor/session';
 
 // Associated template
 import './home-page.html';
@@ -9,15 +10,26 @@ import '../homepage/presentation.js';
 import '../homepage/info-banner.js';
 import '../homepage/theme-item.js';
 
+Template.Home_page.helpers({
+	getAddress: function(){
+		var location = Geolocation.currentLocation().coords;
+		Meteor.call('getLocation', location.latitude, location.longitude, function(err, response) {
+			console.log(response[0]);
+			Session.set('location', response[0]);
+		});
+		return Session.get('location').formattedAddress;
+	}
+});
+
 Template.Home_page.onCreated(function homePageOnCreated() {
-  this.state = new ReactiveDict();
-  Meteor.subscribe('userInfos');
-  Meteor.subscribe('clothes');
+	this.state = new ReactiveDict();
+	Meteor.subscribe('userInfos');
+	Meteor.subscribe('clothes');
 });
 
 Template.Home_page.onRendered(function() {
-  $( document ).ready(function(){
-    // Loading parallax for image blocks
-    $(".parallax").parallax();
-  });
+	$( document ).ready(function(){
+	// Loading parallax for image blocks
+	$(".parallax").parallax();
+	});
 });
--- server/main.js
+++ server/main.js
@@ -1,1 +1,10 @@
 import '/imports/startup/server';
+import { GeoCoder } from 'meteor/aldeed:geocoder';
+
+Meteor.methods({
+	'getLocation': function(latitude, longitude){
+		var geo = new GeoCoder();
+		var result = geo.reverse(latitude, longitude);
+		return result;
+	}
+});
